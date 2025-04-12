71ea41d7af7537c57add756fe67e98b8073eb5fc
rohan sharma dutt <rohansharmadutt876@gmail.com>
2025-04-12 13:45:27 +0530

    api update

--- frontend/src/lib/Axios.js
+++ frontend/src/lib/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
 
-    baseURL:'http://localhost:5000/api',
+    baseURL:'https://viewcount-backend.onrender.com/api',
     withCredentials:true
 })
 
