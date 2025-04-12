c4df990fbaace1d0437405f9e60c8c63a600fdb7
rohan sharma dutt <rohansharmadutt876@gmail.com>
2025-04-12 14:12:29 +0530

    bug cookie

--- backend/middleware/TokenMaker.js
+++ backend/middleware/TokenMaker.js
@@ -9,8 +9,8 @@ export const generateToken = (id,res) =>{
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: true,
+        sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
       });
      return token
